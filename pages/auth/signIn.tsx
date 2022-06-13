128e8b570d37aafa84e4b73cc247831728d0c779
Red-CS <red.devcs@gmail.com>
2022-06-13 00:49:27 -0400

    style: styled sign in page

--- pages/auth/signIn.tsx
+++ pages/auth/signIn.tsx
@@ -1,15 +1,21 @@
 import { NextPage } from "next"
 import { useSession, signIn } from "next-auth/react"
 import { useRouter } from "next/router"
+import GoogleIcon from "@mui/icons-material/Google"
+import styles from "@styles/SignIn.module.sass"
 
 const SignIn: NextPage = () => {
   const { data: session } = useSession()
   const router = useRouter()
   if (session) router.push("/admin")
   return (
-    <div>
-      Hello
-      <button onClick={() => signIn("google")}>Sign in with google</button>
+    <div className={styles.container}>
+      <h1>Access Protected</h1>
+      <h2>Sign In to Continue</h2>
+      <button onClick={() => signIn("google")}>
+        <GoogleIcon />
+        <span>Sign In with Google</span>
+      </button>
     </div>
   )
 }
