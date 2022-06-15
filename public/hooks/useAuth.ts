d9758ef5230817451ddd6b46d77600856931ca75
Red-CS <red.devcs@gmail.com>
2022-06-15 12:57:16 -0400

    docs: doc useAuth

--- public/hooks/useAuth.ts
+++ public/hooks/useAuth.ts
@@ -11,24 +11,36 @@ const useAuth = () => {
   const { status } = useSession()
   const [isAuthenticated, dispatch] = useReducer(reducer, false)
 
-  const authed = useRef(false)
+  /**
+   * `false` if client has no record of being authenticated
+   * during the lifetime of the session,
+   * `true` if there is a record (such as being signed out)
+   *
+   * Used to push client to home vs sign in page
+   */
+  const authedPrior = useRef(false)
 
   useEffect(() => {
     switch (status) {
+      // Maintains that client has been authenticated
       case "authenticated":
         dispatch({ type: "authenticated" })
-        authed.current = true
-        return
+        authedPrior.current = true
+
+      // Pushes route to home or sign in based on prior auth status
       case "unauthenticated":
         dispatch({ type: "unauthenticated" })
-        if (authed.current) {
+        if (authedPrior.current) {
           router.push("/")
-          // authed.current = false
         } else router.push("/auth/signIn")
-        return
+
+      // Loading state, if necessary
       case "loading":
         dispatch({ type: "loading" })
-        return
+
+      // Should never run
+      default:
+        throw new Error(`'status' var held an unexpected value: ${status}`)
     }
   }, [status, router])
 
@@ -38,14 +50,19 @@ const useAuth = () => {
 type State = boolean
 type Action = { type: "authenticated" | "unauthenticated" | "loading" }
 
+/**
+ * `React.useReducer` reducer function to handle state.
+ *
+ * @param state Current state of the component
+ * @param action New state to dispatch
+ * @returns { State } The updated state
+ */
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case "authenticated":
       return true
     case "unauthenticated":
       return false
-    // case "loading":
-    //   return null
   }
   return false
 }
