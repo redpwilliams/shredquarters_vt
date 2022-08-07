709afc42f3ae1513862373d0b6c97ea1a946eb45
Red-CS <red.devcs@gmail.com>
2022-08-07 17:44:23 -0400

    feat: add event api route

--- pages/api/events/create.ts
+++ pages/api/events/create.ts
@@ -1,16 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type { Event } from '@public/types'
 import type { PostgrestResponse } from '@supabase/postgrest-js'
+import type { Keys } from 'pages/admin/events/update'
 import { supabase } from '@db/_supabase'
+import { validate } from '../auth/[...nextauth]'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await validate(req, res)
+
+  const entry: Keys = {
+    name: req.body.name,
+    date: req.body.date,
+    start_time: req.body.start,
+    end_time: req.body.end,
+    location: req.body.location
+  }
   if (req.method === 'POST') {
     const { data, error }: PostgrestResponse<Event> = await supabase
       .from('events')
-      .insert(req.body)
+      .insert(entry)
 
     if (data) {
       return res.status(200).json(data)
