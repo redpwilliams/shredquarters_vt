785bf430ce2f9700699b198b52535fc509592609
Red Williams <red.devcs@gmail.com>
2022-08-29 02:04:07 -0400

    style: OfficerImage fucking text divider

--- components/ui/OfficerImage/OfficerImage.tsx
+++ components/ui/OfficerImage/OfficerImage.tsx
@@ -14,21 +14,25 @@ const OfficerImage = ({ officer }: { officer: Officer }) => {
 
   return (
     <li className={styles.container}>
-      <div className={styles.img}>
-        <Image
-          id='img'
-          src={imageUrl}
-          width={224}
-          height={224}
-          style={{ borderRadius: '5%' }}
-        />
+      <div className={styles.header}>
+        <div className={styles.img}>
+          <Image
+            id='img'
+            src={imageUrl}
+            width={224}
+            height={224}
+            style={{ borderRadius: '5%' }}
+          />
+        </div>
+        <div className={styles.name}>
+          <h1>{officer.first_name}</h1>
+          <h2>{officer.last_name}</h2>
+        </div>
       </div>
-      <div className={styles.left}>
-        <h1>{officer.first_name}</h1>
-        <h2>{officer.last_name}</h2>
-      </div>
-      <div className={styles.right}>
-        <h3>{officer.position}</h3>
+      <div className={styles.info}>
+        <h3>
+          <span>{officer.position}</span>
+        </h3>
         <p>{officer.bio}</p>
       </div>
     </li>
