78ac85504dec55c92be56c496161cbfdf905ec7b
Red Williams <red.devcs@gmail.com>
2022-09-01 19:00:49 -0400

    feat: SideMenu links, SignIn mobile spacing

--- components/ui/SideMenu/SideMenu.tsx
+++ components/ui/SideMenu/SideMenu.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { MenuContext, AuthContext } from '@components/context'
 import { useWindowSize } from '@components/hooks'
+import Link from 'next/link'
 import styles from './SideMenu.module.sass'
 
 const SideMenu = () => {
@@ -27,16 +28,20 @@ const SideMenu = () => {
 const Menu = () => (
   <ol className={styles.menu}>
     <li>
-      <span>1. </span>Events
+      <span>1. </span>
+      <Link href='#plan'>Events</Link>
     </li>
     <li>
-      <span>2. </span>Officers
+      <span>2. </span>
+      <Link href='#team'>Officers</Link>
     </li>
     <li>
-      <span>3. </span>Contact
+      <span>3. </span>
+      <Link href='#network'>Contact</Link>
     </li>
     <li>
-      <span>4. </span>Admin
+      <span>4. </span>
+      <Link href='/admin'>Admin</Link>
     </li>
   </ol>
 )
@@ -46,20 +51,36 @@ const AuthMenu = () => (
     <h1>SQ Admin</h1>
     <h2>Events</h2>
     <ol>
-      <li>Create an Event</li>
-      <li>Edit an Event</li>
-      <li>Delete an Event</li>
+      <li>
+        <Link href='/admin/events/create'>Create an Event</Link>
+      </li>
+      <li>
+        <Link href='/admin/events/delete'>Edit an Event</Link>
+      </li>
+      <li>
+        <Link href='/admin/events/delete'>Delete an Event</Link>
+      </li>
     </ol>
     <h2>Officers</h2>
     <ol>
-      <li>Submit a new Officer</li>
-      <li>Edit an existing Officer</li>
-      <li>Remove an existing Officer</li>
+      <li>
+        <Link href='/admin/officers/delete'>Submit a new Officer</Link>
+      </li>
+      <li>
+        <Link href='/admin/officers/delete'>Edit an existing Officer</Link>
+      </li>
+      <li>
+        <Link href='/admin/officers/delete'>Remove an existing Officer</Link>
+      </li>
     </ol>
     <h2>Admin Users</h2>
     <ol>
-      <li>Add a new Administrator</li>
-      <li>Remove an existing Administrator</li>
+      <li>
+        <Link href='/admin/users/delete'>Add a new Administrator</Link>
+      </li>
+      <li>
+        <Link href='/admin/users/delete'>Remove an existing Administrator</Link>
+      </li>
     </ol>
   </div>
 )
