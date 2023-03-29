52a6cc2859383d8c86e16b476b89db652fc86d9d
Red Williams <redpwilliams@gmail.com>
2023-03-28 23:39:58 -0400

    feat: add skeleton of 404

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import styles from '@styles/Signin.module.sass'
+
+const Custom404 = () => (
+  <div className={styles.container}>
+    <h1>404</h1>
+    <h2>Page not Found!</h2>
+  </div>
+)
+
+export default Custom404
